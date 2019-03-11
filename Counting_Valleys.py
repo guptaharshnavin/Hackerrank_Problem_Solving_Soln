262ea9b048419abad4bc92fcc03ffc04885040b1
Harsh Navin Gupta <47296820+guptaharshnavin@users.noreply.github.com>
2019-03-11 21:37:19 +0530

    Add files via upload

---
+++ Counting_Valleys.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    current_level = 0
+    valley_count = 0
+
+    for step in s:
+        if(step == 'U'):
+            current_level += 1
+            if(current_level == 0):
+                valley_count += 1
+        else:
+            current_level -= 1
+    return valley_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
