c44ac522a3d04137936b7b5283c2ab356f401cf2
Harsh Navin Gupta <47296820+guptaharshnavin@users.noreply.github.com>
2020-02-05 20:10:49 +0530

    Update Repeated_String.py

--- Repeated_String.py
+++ Repeated_String.py
@@ -8,30 +8,17 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
-    print(f'S : {s}')
-    print(f'N : {n}')
-    search_count = 0
-    total_count = 0
-
-    for c in s:
-        if c == 'a' or c == 'A':
-            search_count += 1
-    print(f'Search Count : {search_count}')
-    length = len(s)
-    m_len = int(n/length)
-    a_len = (length * m_len) + 1
-    total_count = search_count * m_len
+    str_len = len(s)
+    mul_floor = math.floor(n / str_len)
+    org_count = (s.count("a")) * mul_floor
     
-    index = 0
-    while a_len <= n:
-        if index == length:
-            index = 0
-        if s[index] == 'A' or s[index] == 'a':
-            total_count += 1
-        a_len += 1
-        index += 1
-    return int(total_count)
+    rem_len = n - (str_len * mul_floor)
 
+    if rem_len != 0:
+        new_str = s[:rem_len]
+        org_count = org_count + new_str.count("a")
+
+    return(org_count)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
